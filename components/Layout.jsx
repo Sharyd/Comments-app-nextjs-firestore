201b9963ceab3e9f83371d88a05f001dae78781b
Daniel Vacek <Raimen77@seznam.cz>
2022-10-17 02:10:42 +0200

    centered

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from "./Nav";
 const Layout = ({ children }) => {
   return (
-    <div className="mx-6 md:max-w-2xl md:max-auto font-poppins">
+    <div className="flex flex-col md:ml-auto md:mr-auto mx-6 md:max-w-2xl md:max-auto font-poppins">
       <Nav />
       <main>{children}</main>
     </div>
